8b08901ac7ca1f004da3f29a4690db5b1e52fc33
Nel Cesar <70825913+ncesar21@users.noreply.github.com>
2022-02-24 22:18:55 +0000

    Update app.py

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ def home():
 return '<h1> index page </h1>'
 @app.route("/home")
 def helloworld():
-return '<h1> Hello world</h1><title> <h1> we are learning Flask </h1>'
+return '<h1> Hello Team</h1><title> <h1> Welcome to Fantasy Football </h1>'
